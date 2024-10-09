d111fc7aa653c5284dd7b8da2f343ee8dc01f0cc
smith <smithbabawande@gmail.com>
2024-10-09 12:58:49 +0100

    we move

--- src/componenents/explorecomp/Explore.tsx
+++ src/componenents/explorecomp/Explore.tsx
@@ -16,48 +16,52 @@ function Explore() {
       <div>
         <h1>Presidential Election</h1>
         <div className='flex justify-evenly flex-wrap mt-5 md:gap-5'>
-          <VoteComp name='Peter Obi' img={obi} />
-          <VoteComp name='Bola Tinubu' img={bola} />
-          <VoteComp name='Peter Obi' img={obi} />
-          <VoteComp name='Bola Tinubu' img={bola} />
-          <div className=' w-fit bg-blue-200 p-5 text-left text-sm'>
+          <VoteComp name='Peter Obi' img={obi} party='LP'/>
+          <VoteComp name='Bola Tinubu' img={bola} party='APC' />
+          <VoteComp name='Peter Obi' img={obi} party='LP'/>
+          <VoteComp name='Bola Tinubu' img={bola} party='APC'/>
+        </div>
+        <div className='flex justify-evenly align-center w-full bg-blue-200 p-5 text-left text-sm mt-5'>
             <ul>
               <li>{account.address} voted for obi</li>
             </ul>
-          </div>
-        </div>
+          </div>  
       </div>
-      <div className=' mt-20'>
+      <div className='grid mt-20'>
         <h1>
           Governnorship Election
         </h1>
         <div className='flex justify-evenly align-center flex-wrap mt-5 md:gap-5'>
-          <VoteComp name='Datti-Baba' img={datti} />
-          <VoteComp name='Sanwo-Olu' img={sanwo} />
-          <VoteComp name='Datti-Baba' img={datti} />
-          <VoteComp name='Sanwo-Olu' img={sanwo} />
-          <div className=' w-fit bg-blue-200 p-5 text-left text-sm'>
+          <VoteComp name='Datti-Baba' img={datti} party='LP'/>
+          <VoteComp name='Sanwo-Olu' img={sanwo} party='APC'/>
+          <VoteComp name='Datti-Baba' img={datti} party='LP'/>
+          <VoteComp name='Sanwo-Olu' img={sanwo} party='APC'/>
+        </div>
+        <div className='flex justify-evenly align-center w-full bg-blue-200 p-5 text-left text-sm mt-5'>
             <ul>
               <li>{account.address} voted for datti</li>
             </ul>
           </div>
-        </div>
       </div>
+
+          
+      
       <div className=' mt-20'>
         <h1>
           House of Reps Election
         </h1>
         <div className='flex justify-evenly align-center flex-wrap mt-5 md:gap-5'>
-          <VoteComp name='Datti-Baba' img={datti} />
-          <VoteComp name='Sanwo-Olu' img={sanwo} />
-          <VoteComp name='Datti-Baba' img={datti} />
-          <VoteComp name='Sanwo-Olu' img={sanwo} />
-          <div className=' w-fit bg-blue-200 p-5 text-left text-sm'>
+          <VoteComp name='Datti-Baba' img={datti} party='LP'/>
+          <VoteComp name='Sanwo-Olu' img={sanwo} party='APC'/>
+          <VoteComp name='Datti-Baba' img={datti} party='LP'/>
+          <VoteComp name='Sanwo-Olu' img={sanwo} party='APC'/>
+        </div>
+        <div className='flex justify-evenly align-center w-full bg-blue-200 p-5 text-left text-sm mt-5'>
             <ul>
               <li>{account.address} voted for datti</li>
             </ul>
           </div>
-        </div>
+          
       </div>
     </div>
   )
--- src/componenents/homecomp/Home.tsx
+++ src/componenents/homecomp/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { useAccount } from 'wagmi'
 import baselogo from '../../assets/baselogo.png'
+import RetroGrid from '../ui/retro-grid';
 
 interface MyObject{
   [key: string]: string,
@@ -40,22 +41,38 @@ function Home() {
 
 
   return (
-    <div>
-      <h1>Welcome to OnchainVote</h1>
-      <p className='mb-10 text-xl'>A voting system built on BASE to promote a more secure and transparent way of voting.</p>
-      <div className='z-10 text-xl'>
+    <div className=''>
+      {/* hero section */}
+    <div className='relative flex flex-col items-center justify-center w-full min-h-screen px-3 overflow-hidden bg-background'>
+    <div className="text-center">
+    <h1 className='-mt-30 pt-26 text-5xl font-bold text-black md:text-8xl ml-6'>Welcome to{" "} <span  className="text-blue-600">OnchainVote</span></h1> 
+
+    <p className="mt-4 text-lg text-slate-700 md:text-xl">A voting system built on BASE to promote a more secure and transparent way of voting.</p>
+    </div>
+     
+      <div className='z-10 text-xl mt-4'>
         <div className='flex justify-center items-center flex-col'>
-          <input type="text" name='pvcnumber' placeholder='Enter your pvc number' className='p-2 mb-2 mr-2 border rounded-xl border-blue-500 outline-none text-center' onChange={handleUsrInp} value={usrInp} />
+          <input type="text" name='pvcnumber' placeholder='Enter your pvc number' className='p-2 mt-2 mb-2 mr-2 border rounded-xl border-blue-500 outline-none text-center' onChange={handleUsrInp} value={usrInp} />
           <p className='text-sm'>ID will be mapped to your address: {account.address}</p>
         </div>
         <button className='m-6 cursor-pointer b-1 border rounded-xl border-blue-500 outline-none' onClick={handleAccreditate} disabled={isDissabled}>Accreditate</button>
+      
       </div>
+      
       <div>
-        <img src={baselogo} alt="baselogo" className='w-48 absolute top-32 left-5' />
-        <img src={baselogo} alt="baselogo" className='w-48 absolute bottom-32 right-5' />
+        <img src={baselogo} alt="baselogo" className='w-48 absolute top-20 left-0' />
+        <img src={baselogo} alt="baselogo" className='w-48 absolute top-20 right-5 ' />
+        <img src={baselogo} alt="baselogo" className='w-48 absolute bottom-12 right-5 ' />
+        <img src={baselogo} alt="baselogo" className='w-48 absolute bottom-12 left-0 ' />
       </div>
+
+      <RetroGrid />
+    </div>
+   
     </div>
   )
 }
+     
+      
 
 export default Home
